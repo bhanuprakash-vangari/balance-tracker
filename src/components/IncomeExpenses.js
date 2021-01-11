4ea30ec524b1d84fa2ed37848d46a33a885e12f6
bhanu6143 <32790372+bhanu6143@users.noreply.github.com>
2021-01-11 21:06:47 +0530

    added useState Hook

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -5,14 +5,14 @@ export const IncomeExpenses = () => {
       <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p id="money-plus" className="money plus">
-            +$0.00
+          <p className="money plus">
+            +₹0.00
           </p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p id="money-minus" className="money minus">
-            -$0.00
+          <p className="money minus">
+            -₹0.00
           </p>
         </div>
       </div>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,7 +4,7 @@ export const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul id="list" class="list">
+      <ul id="list" className="list">
         <li className="minus">
           Cash <span>-$400</span>
           <button className="delete-btn">x</button>
