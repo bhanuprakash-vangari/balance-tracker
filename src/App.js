4e413d7ab1343643d122b97199de8a6556664d41
bhanu6143 <32790372+bhanu6143@users.noreply.github.com>
2021-01-11 22:51:06 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <Balance />
+    <GlobalProvider>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Balance />
+        </div>
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-      <IncomeExpenses />
-      <TransactionList />
-      <AddTransaction />
-    </div>
+    </GlobalProvider>
   );
 }
 
